9653d1b567a4227033964a7835bb5cdde536a5b7
mshakhan <mshakhan@mshakhan-eeepc.(none)>
2008-12-04 22:48:58 +0300

    xml.rb and singleton.rb

---
+++ singleton.rb
@@ -0,0 +1,27 @@
+module Singleton
+  def self.append_features(base)
+    super
+    base.extend(ClassMembers)
+    base.class.send :alias_method, :__new__, :new
+    base.class.send :private, :__new__
+  end  
+
+  module ClassMembers
+    def new
+      @@inst ||= self.send :__new__
+    end 
+  end
+end
+
+
+class C
+  include Singleton
+
+  attr_accessor :state
+  def initialize
+    puts "class #{self.class} constructor. state = #{state}"
+  end
+end
+
+C.new
+C.new # => ?
