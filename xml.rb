cb10e243733efc0b4afc704d549f543eeec679b9
mshakhan <mshakhan@gmail.com>
2009-01-09 02:34:12 -0700

    refactoring

--- xml.rb
+++ xml.rb
@@ -1,104 +1,45 @@
-=begin
-  Attempt to create a xml markup DSL
-  The result should be like that:
-  XmlDoc.new do
-    html do
-      head do
-      end
-    end
+class XmlDoc
+  def initialize(out=$stdout, &blk)
+    @out = out
+    instance_eval(&blk) if blk
   end
-  (without parameter in block)
-=end
-
-class XmlNode
-  attr_accessor :name, :parent, :children, :attrs, :level
   
-  def initialize(name, parent = nil, attrs = {}, level = 0, children = [])
-    @name, @parent, @children, @attrs, @level = name, parent, children, attrs, level
-    @builder = XmlBuilder.new(self)
-    yield self if block_given?
-  end
-
   def method_missing(name, attrs={}, &blk)
-    name = name.to_s
-    node = XmlNode.new(name, self, attrs, @level + 1, &blk)
-    @children << node
-    node
+    out "<#{name}#{self.class.attrs_to_xml attrs}>"
+    instance_eval(&blk) if blk
+    out "</#{name}>"
   end
-
-  def **(str)
-    @children << @builder.text(str)
+    
+  def _(text=nil)
+    out text
   end
-
-  def next
-    return false unless @parent
-    indx = @parent.children.index(self) + 1
-    if indx < @parent.children.size
-      @parent.children[indx]
-    end    
+  
+  def out(text)
+    @out << "#{text}\n"
   end
-
-  def prev
-    return false unless @parent
-    indx = @parent.children.index(self) - 1
-    if indx > 0
-      @parent.children[indx]
-    end        
-  end
-
-  def to_s
-    @builder.build
-  end
-end
-
-class XmlBuilder
-  def initialize(node)
-    @node = node
-  end
-
-  def build
-    children = @node.children.inject([]) do |arr, node|
-      arr << "#{node.to_s}\n"
-    end
-    "#{level_spaces}<#{@node.name}#{build_attrs}>\n#{children.join}#{level_spaces}</#{@node.name}>"    
-  end
-
-  def text(txt)
-    "#{level_spaces 1}#{txt}".gsub('<', '&lt;').gsub('>', '&gt;') 
-  end 
-
-  private
-  def build_attrs
-     @node.attrs.inject('') do |str, arg|
-      str << " #{arg.first}='#{arg.last}'"
-    end     
-  end
-
-  def level_spaces(add = 0)
-    ' ' * (@node.level + add)
-  end
-end
-
-class XHtmlDoc <  XmlNode
-  def initialize
-    super('html')
-  end
-end
-
-# check this
-html = XHtmlDoc.new do |html|
-  html.head do |head|
-    html.title do |title|
-      title ** 'hello from ruby!'
+  
+  def self.attrs_to_xml(attrs)
+    attrs.inject('') do |xml, (k, v)|
+      xml << " #{k}='#{v}'"
     end
   end
-  html.body :align => 'left', :width => '100%' do |body|
-    body ** 'header'
-      body.div :style => 'width:100%; text-align:center;' do |div|
-        div ** 'main content' 
-      end
-    body ** 'footer'
-  end
 end
 
-puts html.to_s
+xml = ''
+XmlDoc.new(xml) {
+  html {
+    head {
+      title {
+        _ 'hello from ruby'
+      }
+    }
+    body {
+      _ 'hello'
+      div(:class => 'fuck') {
+        _ 'hi!'
+      }
+    }
+  }
+}
+
+puts xml
