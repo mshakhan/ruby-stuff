2ed1cc6eaf9b922d003a6b92440ec6a553d87832
mshakhan <mshakhan@gmail.com>
2009-01-10 02:26:52 -0700

    some DRYing

--- model.rb
+++ model.rb
@@ -1,57 +1,56 @@
 #
 # Simple imitation of ActiveRecord::Base behaivour
 #
-
+ 
 class Model
   attr_accessor :attributes
-
+ 
   # Sets the collection of valid model attributes
-  # e.g 
+  # e.g
   # class MyModel < Model
-  # 	valid_attributes :attr1, :attr2
+  #   valid_attributes :attr1, :attr2
   # end
   #
   # m1 = Model1.new
   # m1.attr1 = 10 # => OK
   # a2 = m1.attr2 # => OK
-  # m1.attr3 = 0  # => raises NameError
-  def self.valid_attributes(*attrs)
-    @@valid_attributes = attrs
+  # m1.attr3 = 0 # => raises NameError
+  def self.valid_attributes(*attributes)
+    @@valid_attributes = attributes
   end
-
-  def initialize(attrs = {})
-    @attributes = attrs
+ 
+  def initialize(attributes = {})
+    @attributes = attributes
   end
-
+ 
   alias_method :__method_missing__, :method_missing
-  def method_missing(id, value = nil)
-  # TODO: refactor this shit!
+  def method_missing(id, value=nil)
     name = id.to_s
-    is_setter = (name =~ /=$/)
-    name = name[0..name.size-2] if is_setter
+    is_setter = name.sub!(/=/, '')
+    name = name.to_sym
     if @@valid_attributes
-      __method_missing__(id) unless @@valid_attributes.include?(name.to_sym)
+      __method_missing__(id) unless @@valid_attributes.include? name
     end
     # simple 'caching'
     Model.class_eval <<METH
-      def #{id}#{'(v)' if is_setter}
-        @attributes[:#{name}]#{ ' = v' if is_setter }
-      end
+def #{id}#{'(v)' if is_setter}
+  @attributes[:#{name}]#{ ' = v' if is_setter }
+end
 METH
     if is_setter
-      @attributes[name.to_sym] = value
+      @attributes[name] = value
     else
-      @attributes[name.to_sym]
+      @attributes[name]
     end
   end
-
-  # pretty view of model attributes
+ 
   def inspect
-    @attributes.inject('') do |dump, attr|
-      dump << "#{attr.first} = #{attr.last}\n"
+    attrs = @attributes.inject('') do |dump, attr|
+      dump << "#{attr.first} = #{attr.last} "
     end
+    "#<#{self.class.name}:0x#{self.object_id.to_s(16)} #{attrs}>"
   end
 end
-
+ 
 m = Model.new(:a => 1)
 puts m.inspect
