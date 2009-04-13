480b3c9c92604e76d86a814bda0392eb0de64478
mshakhan <mshakhan@gmail.com>
2009-04-13 02:00:34 -0700

    Conceptually new design =)

--- query.rb
+++ query.rb
@@ -1,102 +1,137 @@
-=begin
-Sql query builing DSL
-E.g:
-:f1.eq(3).or(:f2.ne(4))
-# => f1 = 3 or f2 != 4
-:f1.eq(3).and(:f2.eq(4).or(:f2.eq(5)))
-# => f1 = 3 and (f2 = 4 or f2 = 5)
-:f1.eq(3).and(:f2.eq(4)).or(:f2.eq(5))
-# => f1 = 3 and (f2 = 4) or f2 = 5
-:f1.eq(3).and(:f2.eq(4)).or(:f2.eq(5)).and(:f4.like('%hello%')).or(:f5.in('1', '2', '3'))
-# => f1 = 3 and (f2 = 4) or f2 = 5 and (f4 like '%hello%') or f5 in ('1','2','3')
-=end
-
-# core extensions
-class Symbol
-  def eq(value)
-    Condition.new(:eq, self, value)
+module Sql
+  class Database
+    # Simple mocking database object (create, drop, alter actions needs to be added)
+    def execute(query)
+      puts query.build_sql
+    end 
   end
 
-  def ne(value)
-    Condition.new(:ne, self, value)
-  end
+  class Query
+    attr_accessor(
+      :select_fields,
+      :from_tables,
+      :joins,
+      :conditions,
+      :order            
+    )
 
-  def like(value)
-    Condition.new(:like, self, value.quotize)
-  end
+    def initialize(&blk)
+      self.joins = {}
+      instance_eval(&blk)
+    end
+    
+    def select(*fields)
+      self.select_fields = fields
+    end
 
-  def in(*values)
-    Condition.new(:in, self, "(#{values.collect{|v| v.quotize }.join(',')})")
-  end
-end
+    def from(*tables)
+      self.from_tables = tables
+    end
 
-class Object
-  def quotize
-    return self unless self.is_a? String
-    "'#{self}'"
-  end
-end
-# end of core extensions
+    def join(table)
+      @join_table = table
+    end
 
-# Elementary condition like field1 = 1
-# Includes collection of child conditions
-class Condition
-  attr_reader :type, :field, :value, :children
-  attr_accessor :op
+    def on(conditions)
+      if @join_table
+        self.joins[@join_table] = conditions
+        @join_table = nil
+      else
+        raise "Unexpected ON"
+      end
+    end
 
-  def initialize(type, field, value, op = nil)
-    @type, @field, @value, @op = type, field, value, op
-    @children = []
-  end
+    def where(conditions)
+      self.conditions = conditions
+    end
 
-  def and(cond)
-    cond.op = :and
-    @children << cond
-    self
-  end
+    def order_by(*fields)
+      self.order = fields
+    end
 
-  def or(cond)
-    cond.op = :or
-    @children << cond
-    self
-  end
+    def build_sql
+      unless @sql
+        @sql = ''
+        @sql << "SELECT #{self.select_fields.flatten.join(',')}\n"
+        @sql << "FROM #{self.from_tables.join(',')}\n"
+        @sql << self.joins.inject('') do |acc, (table, join_conditions)|
+          acc << "JOIN #{table} ON #{join_conditions}\n"
+          acc
+        end
+        @sql << "WHERE #{self.conditions}\n" if self.conditions
+        @sql << "ORDER BY #{self.order.join(',')}\n" if self.order
+        @sql
+      else
+        @sql
+      end
+    end
 
-  def build
-    SqlBuilder.new(self).build
-  end
-  alias_method :to_s, :build
-end
+    protected
+    def method_missing(id)
+      Proxy.new(id.to_s)
+    end
 
-# Buils sql WHERE clause from conditions tree
-class SqlBuilder
-  def initialize(cond)
-    @cond = cond
-  end
+    class Proxy
+      attr_accessor :text, :parent
 
-  def build
-    dump = "#{@cond.field} #{type_to_sym(@cond.type)} #{@cond.value}"
-    @cond.children.inject(dump) do |dmp, cond|
-      dump << append_op(cond)
-    end.strip
-  end
+      def initialize(text)
+        self.text = text
+      end
+      
+      METH_OP_MAP = {
+        '==' => '=',
+        '>' => '>',
+        '<' => '<',
+        '<=>' => '<>',
+        '&' => 'AND',
+        '|' => 'OR',
+        'like' => 'LIKE'
+      }
 
-  private
-  def type_to_sym(type)
-    # TODO: in constant
-    {
-      :eq => '=',
-      :ne => '!=',
-      :in => 'in',
-      :like => 'like'
-    }[type]
-  end
+      METH_OP_MAP.each_pair do |meth, op|
+        define_method meth do |value|
+          Proxy.new("#{self.text} #{op} #{quotize_value(value)}")
+        end
+      end
 
-  def append_op(cond)
-    case cond.op
-    when :or
-      " or #{cond.build}"
-    when :and
-      " and (#{cond.build})"
+      def in(*values)
+        values = values.map { |value| quotize_value(value) }.join(',')
+        Proxy.new("#{self.text} IN(#{values})")
+      end
+
+      def g
+        self.text = "(#{self.text})"
+        self
+      end
+
+      def method_missing(id)
+        Proxy.new("#{self.text}.#{id}")
+      end
+
+      def to_s
+        self.text
+      end
+
+      protected
+      def quotize_value(value)
+        if [String, Symbol].include?(value.class)
+          "'#{value}'"
+        else
+          value
+        end
+      end
     end
   end
 end
+
+query = Sql::Query.new do
+  select table1.*, table2.field13
+  from table1
+  #join table2
+  #on table1.field10 == table2.field10
+  where ((table1.field1.like('asd')) | (table2.field2 <=> 2)).g & (field3.in(1, 2, 3))
+  #order_by field1, field2
+end
+
+db = Sql::Database.new
+db.execute(query)
